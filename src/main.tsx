6e6eca632a623371194f4b1b55abf84e9896fe70
Jonatan <jonatanav255@hotmail.com>
2025-02-28 12:22:11 -0600

    Revert "deploying chat"
    This reverts commit b17d4fe6d2834565c41c9d5990fb42eeee2d0bf1.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Chat from './Chat/Chat.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div style={{display:"flex", gap:"10px"}}>
-      <App />
-      <Chat />
-    </div>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
