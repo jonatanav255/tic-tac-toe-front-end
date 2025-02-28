b17d4fe6d2834565c41c9d5990fb42eeee2d0bf1
Jonatan <jonatanav255@hotmail.com>
2025-02-28 11:48:43 -0600

    deploying chat

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Chat from './Chat/Chat.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <div style={{display:"flex", gap:"10px"}}>
+      <App />
+      <Chat />
+    </div>
+  </StrictMode>
 )
